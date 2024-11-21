b0bc931b03fdee6dcc397096107cc3462857ea6d
NelliMorelGarcia <nellimorel323@gmail.com>
2024-11-21 15:48:11 -0500

    Add files via upload

---
+++ homep.php
@@ -0,0 +1,20 @@
+        <?php
+?>
+<section>
+<body>
+    <header>
+    <h2>Welcome to the NGF Gym.</h2>
+
+        <p>Take some time to get to know the best gym in the city.</p>
+     <p>This gym have the best equipment and environment you need for a great workout.</p>
+     <p>We specialize in bodybuilders, powerlifters and athletes.</p>
+     <p>We look forward to hearing from you to apply for a membership at this gym.</p>
+  </section>
+
+
+</form>
+</section>
+<?php
+
+?>
+
